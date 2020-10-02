06b49d999c4d345145ce1256b963813630c89777
Jonathan Lin <jonathan.fs.lin@gmail.com>
2020-10-01 22:20:00 -0400

    updated loadtrialinfo

--- Analysis/IOCAnalysis03_gpGen.m
+++ Analysis/IOCAnalysis03_gpGen.m
@@ -1,7 +1,9 @@
 %restructure the mat file for GP analysis
 %% load the mat in question
-loadFilePath = 'D:\results\fatigue_ioc03_weightsPattern\20200413_FatigueFull_3CF_3\mat\mat_Subject02_3DOF_3CF.mat';
+loadFilePath = 'C:\Users\jf2lin\Downloads\TransferXL-00YNMdn2vnv70\png\mat\mat_Subject14_3DOF_3CF.mat';
 load(loadFilePath);
+
+setPaths();
 
 %% rearange into x = rep, y = features
 % pull out the q information
@@ -13,8 +15,69 @@ fatigueScore = segOnlyDataTable.fatigueScore;
 y = [];
 y_label = {};
 y_inds = 5:22;
+% for i = 1:3
+%     currStats = stats_q(i);
+%     segStats_SingleWindow = currStats.segStats_SingleWindow;
+%     [segOnlyDataTable, restOnlyDataTable, segMask, restMask] = sepSegRest(segStats_SingleWindow);
+%     
+%     x = 1:size(segOnlyDataTable, 1);
+%     y_fieldNames = segOnlyDataTable.Properties.VariableNames;
+%     y_seg = table2array(segOnlyDataTable(:, y_inds));
+%     y = [y y_seg];
+%     for j = y_inds
+%         y_new_label = [currStats.name '_' y_fieldNames{j}];
+%         y_label = [y_label y_new_label];
+%     end
+% end
+% 
+% for i = 1:3
+%     currStats = stats_dq(i);
+%     segStats_SingleWindow = currStats.segStats_SingleWindow;
+%     [segOnlyDataTable, restOnlyDataTable, segMask, restMask] = sepSegRest(segStats_SingleWindow);
+%     
+%     x = 1:size(segOnlyDataTable, 1);
+%     y_fieldNames = segOnlyDataTable.Properties.VariableNames;
+%     y_seg = table2array(segOnlyDataTable(:, y_inds));
+%     y = [y y_seg];
+%     for j = y_inds
+%         y_new_label = [currStats.name '_' y_fieldNames{j}];
+%         y_label = [y_label y_new_label];
+%     end
+% end
+% 
+% for i = 1:3
+%     currStats = stats_tau(i);
+%     segStats_SingleWindow = currStats.segStats_SingleWindow;
+%     [segOnlyDataTable, restOnlyDataTable, segMask, restMask] = sepSegRest(segStats_SingleWindow);
+%     
+%     x = 1:size(segOnlyDataTable, 1);
+%     y_fieldNames = segOnlyDataTable.Properties.VariableNames;
+%     y_seg = table2array(segOnlyDataTable(:, y_inds));
+%     y = [y y_seg];
+%     for j = y_inds
+%         y_new_label = [currStats.name '_' y_fieldNames{j}];
+%         y_label = [y_label y_new_label];
+%     end
+% end
+% 
+% for i = 1:3
+%     currStats = stats_dtau(i);
+%     segStats_SingleWindow = currStats.segStats_SingleWindow;
+%     [segOnlyDataTable, restOnlyDataTable, segMask, restMask] = sepSegRest(segStats_SingleWindow);
+%     
+%     x = 1:size(segOnlyDataTable, 1);
+%     y_fieldNames = segOnlyDataTable.Properties.VariableNames;
+%     y_seg = table2array(segOnlyDataTable(:, y_inds));
+%     y = [y y_seg];
+%     for j = y_inds
+%         y_new_label = [currStats.name '_' y_fieldNames{j}];
+%         y_label = [y_label y_new_label];
+%     end
+% end
+
 for i = 1:3
-    segStats_SingleWindow = stats_q(i).segStats_SingleWindow;
+    currStats = stats_weights(i);
+    segStats_SingleWindow = currStats.segStats_SingleWindow;
     [segOnlyDataTable, restOnlyDataTable, segMask, restMask] = sepSegRest(segStats_SingleWindow);
     
     x = 1:size(segOnlyDataTable, 1);
@@ -22,11 +85,31 @@ for i = 1:3
     y_seg = table2array(segOnlyDataTable(:, y_inds));
     y = [y y_seg];
     for j = y_inds
-        y_new_label = [stats_q(i).name '_' y_fieldNames{j}];
+        y_new_label = [currStats.name '_' y_fieldNames{j}];
         y_label = [y_label y_new_label];
     end
 end
 
- 
+for i = 1:3
+    currStats = stats_dweights(i);
+    segStats_SingleWindow = currStats.segStats_SingleWindow;
+    [segOnlyDataTable, restOnlyDataTable, segMask, restMask] = sepSegRest(segStats_SingleWindow);
+    
+    x = 1:size(segOnlyDataTable, 1);
+    y_fieldNames = segOnlyDataTable.Properties.VariableNames;
+    y_seg = table2array(segOnlyDataTable(:, y_inds));
+    y = [y y_seg];
+    for j = y_inds
+        y_new_label = [currStats.name '_' y_fieldNames{j}];
+        y_label = [y_label y_new_label];
+    end
+end
+
 y = [y fatigueScore];
 y_label = [y_label 'fatigueScore'];
+
+saveVar.x = x;
+saveVar.y = y;
+saveVar.y_label = y_label;
+
+% save('saveVar.mat', 'saveVar');
