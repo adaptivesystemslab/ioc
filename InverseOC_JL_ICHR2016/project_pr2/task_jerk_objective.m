9fee064e81ddb3b6af161fd634d7c7668cbd629e
Maram-Sakr <58448463+Maram-Sakr@users.noreply.github.com>
2020-10-21 18:24:11 -0700

    Add files via upload

--- InverseOC_JL_ICHR2016/project_pr2/task_jerk_objective.m
+++ InverseOC_JL_ICHR2016/project_pr2/task_jerk_objective.m
@@ -1,12 +1,15 @@
-function cost = task_jerk_objective(mdl, q, dq, ddq, index)
+function cost = task_jerk_objective(mdl, q, dq, ddq)
 % Purpose: compute the cost for the task_jerk
 % Input: 1 time index
-% Prereq: run process_data.m (mdl and POSITION need to be loaded)
 
 % Apply the joint angles for the time index
-[pos, ~, ~] = getXandQ(mdl, q, dq, ddq, index);
-x = pos;
+for i = 1:size(q, 2)
+    [pos, ~, ~] = getXandQ(mdl, q, dq, ddq, i);
+    x(:,i) = pos;
+end
+
 dt = 1/100;
+x =  filter_dualpassBW(x', 0.05, 0, 4)';
 
 dx = calcDeriv(x, dt);
 ddx = calcDeriv(dx, dt);
